9534949886101cf45bf33eda3d7574c85bc6fe1f
min4034415 <min4034415@gmail.com>
2024-07-17 13:34:09 +0900

    Initial Commit

---
+++ LogYourLife/LogYourLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogYourLifeApp.swift
+//  LogYourLife
+//
+//  Created by Ouimin Lee on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LogYourLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
