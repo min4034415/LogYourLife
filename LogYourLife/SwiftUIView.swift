e9f36835df9a91df27f39577d2bd1502819e1a86
min4034415 <min4034415@gmail.com>
2024-08-19 17:26:53 +0900

    Story of Night fall

---
+++ LogYourLife/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  LogYourLife
+//
+//  Created by Ouimin Lee on 7/17/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack {
+            Circle()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
+            Text("She Comes in color")
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ LogYourLife/TimeSet.swift
@@ -0,0 +1,8 @@
+//
+//  TimeSet.swift
+//  LogYourLife
+//
+//  Created by Ouimin Lee on 7/17/24.
+//
+
+import Foundation
